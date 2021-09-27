578dc3aa7f6e64ea13399c9948c49cb3024fa094
AKB428 <gandara016+1@gmail.com>
2021-09-27 20:59:39 +0900

    vscode コードフォーマッタで修正

--- kamakiri.py
+++ kamakiri.py
@@ -21,37 +21,37 @@ parser = OptionParser()
 
 parser.add_option(
     '-r', '--register_flag',
-    action = 'store_true',
-    dest = 'register_flag',
-    help = 'DB register mode on'
+    action='store_true',
+    dest='register_flag',
+    help='DB register mode on'
 )
 
 parser.add_option(
     '-s', '--save_image_flag',
-    action = 'store_true',
-    dest = 'save_image_flag',
-    help = 'save image flag'
+    action='store_true',
+    dest='save_image_flag',
+    help='save image flag'
 )
 
 parser.add_option(
     '-y', '--year',
-    action = 'store',
-    type = 'str',
-    dest = 'year',
+    action='store',
+    type='str',
+    dest='year',
 )
 
 parser.add_option(
     '-c', '--cours',
-    action = 'store',
-    type = 'str',
-    dest = 'cours',
+    action='store',
+    type='str',
+    dest='cours',
 )
 
 parser.set_defaults(
-    year = "2016",
-    cours_id = "1",
-    register_switch = False,
-    save_image_flag = False
+    year="2016",
+    cours_id="1",
+    register_switch=False,
+    save_image_flag=False
 )
 
 options, args = parser.parse_args()
@@ -76,35 +76,36 @@ master_ids = []
 
 log_time = datetime.now()
 
+
 def parse_meta_data(bsObj):
 
-    v = bsObj.find("meta", attrs={"name":"description"})
+    v = bsObj.find("meta", attrs={"name": "description"})
     description = v.get("content") if v else ""
 
-    v = bsObj.find("meta", attrs={"name":"keywords"})
+    v = bsObj.find("meta", attrs={"name": "keywords"})
     keywords = v.get("content") if v else ""
 
-    v = bsObj.find("meta", attrs={"name":"author"})
+    v = bsObj.find("meta", attrs={"name": "author"})
     author = v.get("content") if v else ""
 
     # --------- OGP ------------
 
-    v = bsObj.find("meta", attrs={"property":"og:title"})
+    v = bsObj.find("meta", attrs={"property": "og:title"})
     og_title = v.get("content") if v else ""
 
-    v = bsObj.find("meta", attrs={"property":"og:type"})
+    v = bsObj.find("meta", attrs={"property": "og:type"})
     og_type = v.get("content") if v else ""
 
-    v = bsObj.find("meta", attrs={"property":"og:url"})
+    v = bsObj.find("meta", attrs={"property": "og:url"})
     og_url = v.get("content") if v else ""
 
-    v = bsObj.find("meta", attrs={"property":"og:image"})
+    v = bsObj.find("meta", attrs={"property": "og:image"})
     og_image = v.get("content") if v else ""
 
-    v = bsObj.find("meta", attrs={"property":"og:site_name"})
+    v = bsObj.find("meta", attrs={"property": "og:site_name"})
     og_site_name = v.get("content") if v else ""
 
-    v = bsObj.find("meta", attrs={"property":"og:description"})
+    v = bsObj.find("meta", attrs={"property": "og:description"})
     og_description = v.get("content") if v else ""
 
     id = master['id']
@@ -145,6 +146,7 @@ def parse_meta_data(bsObj):
 
     meta_data_list.append(meta_data)
 
+
 for master in master_list:
 
     print(master['title'] + " " + master['public_url'])
@@ -171,10 +173,12 @@ for master in master_list:
 def build_insert_sql(table_name):
     return 'INSERT INTO ' + table_name + ' VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
 
+
 ANIME_API_DB_HOST = os.getenv('ANIME_API_DB_HOST', "localhost")
 ANIME_API_DB_USER = os.getenv('ANIME_API_DB_USER', "root")
 ANIME_API_DB_PASS = os.getenv('ANIME_API_DB_PASS', "")
-ANIME_API_DB_DATABASE = os.getenv('ANIME_API_DB_DATABASE', "anime_admin_development")
+ANIME_API_DB_DATABASE = os.getenv(
+    'ANIME_API_DB_DATABASE', "anime_admin_development")
 
 if register_flag:
     connection = pymysql.connect(host=ANIME_API_DB_HOST,
@@ -186,17 +190,20 @@ if register_flag:
 
     # Statusテーブルを初期化
     with connection.cursor() as cursor:
-        sql = "delete FROM site_meta_data WHERE bases_id IN (" + ",".join(master_ids) + ")"
+        sql = "delete FROM site_meta_data WHERE bases_id IN (" + ",".join(
+            master_ids) + ")"
         print(sql)
         cursor.execute(sql)
-        cursor.executemany(build_insert_sql("site_meta_data"), meta_data_list_db)
-        cursor.executemany(build_insert_sql("site_meta_data_histories"), meta_data_list_db)
+        cursor.executemany(build_insert_sql(
+            "site_meta_data"), meta_data_list_db)
+        cursor.executemany(build_insert_sql(
+            "site_meta_data_histories"), meta_data_list_db)
         connection.commit()
 
 else:
     with open(save_file_name, 'wt') as fout:
-        cout = csv.DictWriter(fout, ['bases_id', 'public_url', 'description','keywords',
-                                     'author','og_title','og_type','og_description','og_url','og_image',
+        cout = csv.DictWriter(fout, ['bases_id', 'public_url', 'description', 'keywords',
+                                     'author', 'og_title', 'og_type', 'og_description', 'og_url', 'og_image',
                                      'og_site_name', 'get_date', 'created_at', 'updated_at'])
         cout.writeheader()
         cout.writerows(meta_data_list)
